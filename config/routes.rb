2872f18fb1967154165fdfbf0db1ab54e151a5f5
ethanryan <ethanryan@gmail.com>
2017-04-10 16:19:28 -0400

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show]
+  #post_path(@post) -- this goes in index.html.erb page, within loop
+  get '/register', to: 'users#new', as: 'register'
+
 end
+
+
+# Example:: Hard coded path: "/posts/#{@post.id}"
+# Example:: Route helper: post_path(@post)
+
+#example of above in console: app.post_path(3)
